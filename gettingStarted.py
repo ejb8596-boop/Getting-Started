c5515edbd2c636b3bd442ff45411759b064a8c83
ejb8596-boop <ejb8596@nyu.edu>
2025-09-05 13:36:12 -0700

    Add files via upload

---
+++ gettingStarted.py
@@ -0,0 +1,25 @@
+import hashlib
+
+def welcome_assignment_answers(question):
+
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "473132bbd2fb61c47c0de04c20dac6bcdfa166c6f830a07583ec06030b284934"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+
+    return answer
+
